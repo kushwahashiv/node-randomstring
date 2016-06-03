aee84c56f65f7379061d637231dc248ef358b596
Shiv Kushwaha <shiv@wizni.com>
2016-06-04 00:38:16 +0530

    Added typescript support

---
+++ lib/randomstring.d.ts
@@ -0,0 +1,20 @@
+export interface Options<T> {
+    length?: number;
+    charset?: Charset;
+    capitalization?: Capitalization;
+    readable?: boolean;
+}
+
+export enum Charset {
+    alphanumeric = 1,
+    numeric,
+    alphabetic,
+    hex
+}
+
+export enum Capitalization {
+    uppercase = 1,
+    lowercase
+}
+
+export declare function generate(options?: Options<Object | number >): string;
